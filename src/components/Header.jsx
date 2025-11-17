5cf68f5f1342cb32d01063efc454556b5b2b4594
Shahzaib <sjobs5245@gmail.com>
2025-11-17 20:17:09 +0500

    Your update message

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,56 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
-export default function Header() {
+import { Link } from "react-router-dom";
 
+export default function Header() {
   return (
     <>
-
       <header className="w-full h-auto bg-blue-600 flex justify-between border-2 border bg-amber-700">
 
-        <div className="w-1/5 border-2 border-amber-400 text-[#ffffff] text-[20px] ml-10">
-          Shahzaib's Collection</div>
+        {/* Logo */}
+        <div className="w-1/5 border-2 border-amber-400 text-white text-[20px] ml-10">
+          <Link to="/">Shahzaib's Collection</Link>
+        </div>
+
+        {/* Navigation */}
         <nav className="w-1/2 border-2 border-amber-400">
           <ul className="h-auto w-full flex justify-evenly">
-            <li className="text-[20px] text-[#ffffff]">Home</li>
-            <li className="text-[20px] text-[#ffffff]">Shop</li>
-            <li className="text-[20px] text-[#ffffff]">Category</li>
-            <li className="text-[20px] text-[#ffffff]">Cart</li>
-            <li className="text-[20px] text-[#ffffff]">Contact</li>
-          </ul></nav><nav />
-        <div className="w-1/5 border-4 border-amber-400 flex justify-end items-center mr-10" >
-          <FontAwesomeIcon icon={faCartShopping} />
-          <FontAwesomeIcon icon={faUser} />
-        </div>
-      </header>
 
+            <li>
+              <Link to="/" className="text-[20px] text-white">Home</Link>
+            </li>
+
+            <li>
+              <Link to="/shop" className="text-[20px] text-white">Shop</Link>
+            </li>
+
+            <li>
+              <Link to="/collection" className="text-[20px] text-white">Collection</Link>
+            </li>
+
+            <li>
+              <Link to="/cart" className="text-[20px] text-white">Cart</Link>
+            </li>
+
+            <li>
+              <Link to="/contact" className="text-[20px] text-white">Contact</Link>
+            </li>
+
+          </ul>
+        </nav>
+
+        {/* Icons Right Side */}
+        <div className="w-1/5 border-4 border-amber-400 flex justify-end items-center mr-10">
+          <Link to="/cart">
+            <FontAwesomeIcon icon={faCartShopping} className="text-white p-1.5" />
+          </Link>
+
+          <Link to="/profile">
+            <FontAwesomeIcon icon={faUser} className="text-white text-[16px]" />
+          </Link>
+        </div>
+
+      </header>
     </>
   );
-
 }
--- src/main/index.jsx
+++ src/main/index.jsx
@@ -1,13 +1,28 @@
 import Header from "../components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export default function Index(){
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+import Shop from "../pages/Shop";
+import Collection from "../pages/Collection";
+import Cart from "../pages/Cart";
+import Login from "../pages/Login";
 
-    return (
-
-        
-        <>
+export default function Index() {
+  return (
+    <>
+      <Router>
         <Header />
-        
-        </>
-    );
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
